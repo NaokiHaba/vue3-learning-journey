13e9e9251aa33461744938c9dcbfc1ededa57b7d
Naoki Haba <naosprintrunner6385@gmail.com>
2024-11-27 03:14:19 +0900

    feat: initial commit

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,6 @@ import pluginVue from 'eslint-plugin-vue'
 import vueTsEslintConfig from '@vue/eslint-config-typescript'
 import pluginVitest from '@vitest/eslint-plugin'
 import oxlint from 'eslint-plugin-oxlint'
-import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
 export default [
   {
@@ -23,5 +22,4 @@ export default [
     files: ['src/**/__tests__/*'],
   },
   oxlint.configs['flat/recommended'],
-  skipFormatting,
 ]
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,20 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import router from './router'
+import { createRouter, createWebHistory } from 'vue-router'
+import { routes } from 'vue-router/auto-routes'
 
 const app = createApp(App)
 
 app.use(createPinia())
+
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
 app.use(router)
 
 app.mount('#app')
